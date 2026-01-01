d502af81fe0e75851d381723598039a9d3ebcb52
Yeshwanth Reddy <my@Mac.lan>
2026-01-01 16:05:40 +0530

    Chore: Add seed data for Jan 1 2026

---
+++ server/db/seed_jan1.sql
@@ -0,0 +1,5 @@
+-- Manual Insert for Jan 1, 2026
+INSERT INTO price_captures (nifty, nasdaq, gold_24k_per_10g, captured_at, capture_time, notes, is_auto_captured)
+VALUES 
+(26173.30, 0, 0, '2026-01-01 09:20:00+05:30', 'morning', 'Manual Entry Jan 1 Open', false),
+(26197.55, 0, 0, '2026-01-01 15:35:00+05:30', 'evening', 'Manual Entry Jan 1 Close', false);
